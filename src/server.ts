f2b78018225bfceade0166320f1555cb17614ee1
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-09-12 20:31:01 -0300

    upload

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from 'fastify';
 import { getAllPrompts } from './routes/get-all-prompts';
+import { uploadVideo } from './routes/upload-video';
 
 const app = fastify();
 
-app.register(getAllPrompts)
+app.register(getAllPrompts);
+app.register(uploadVideo);
 
 app
   .listen({
