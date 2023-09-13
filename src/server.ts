164bc8fa1f5cbe572b2aba20b7658b5b04317963
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-09-13 17:53:12 -0300

    create transcription

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify';
 import { getAllPrompts } from './routes/get-all-prompts';
 import { uploadVideo } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
 
 const app = fastify();
 
 app.register(getAllPrompts);
 app.register(uploadVideo);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
