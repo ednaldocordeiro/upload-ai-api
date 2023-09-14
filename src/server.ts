7d89aaf4672ef14c97ae3748a55062dbc45ca924
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2023-09-14 20:22:06 -0300

    fastify static

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,14 @@ import { getAllPrompts } from './routes/get-all-prompts';
 import { uploadVideo } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-transcription';
 import { generateAICompletionRoute } from './routes/generate-ai-completion';
+import { resolve } from 'node:path';
 
 const app = fastify();
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../tmp'),
+  prefix: '/tmp',
+})
 
 app.register(fastifyCors, {
   origin: '*', // endereço de onde estará hospedado o front
